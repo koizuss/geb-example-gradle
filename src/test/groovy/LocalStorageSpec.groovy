e2526b3501060db285500178a9904838b2a5cb58
Takeshi Koizumi <takeshi_koizumi@kronos-jp.net>
2017-04-05 09:12:27 +0900

    wip

---
+++ src/test/groovy/LocalStorageSpec.groovy
@@ -0,0 +1,14 @@
+import geb.spock.GebReportingSpec
+
+class LocalStorageSpec extends GebReportingSpec {
+
+    def "set item"() {
+      given:
+      go ""
+
+      when:
+      js.exec "window.localStorage.setItem('dummy', 'dummy');"
+
+      then: true
+    }
+}
